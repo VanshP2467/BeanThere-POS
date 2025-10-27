f31d03a5d4db9df0eb529cd2e6ee453b2b79bf2d
Vansh <V.Panchalwar2647@student.leedsbeckett.ac.uk>
2025-10-27 23:40:32 +0000

    [Backend] DB: Map the Base class as Dataclass

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import (
+    sessionmaker,
+    MappedAsDataclass,
+    DeclarativeBase,
+)
 
 # SQLite file in the backend folder
 DATABASE_URL = "sqlite:///./beanthere.db"
@@ -8,4 +12,7 @@ engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}, echo=False
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    pass
